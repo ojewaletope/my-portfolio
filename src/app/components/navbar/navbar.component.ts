b14b882f09e3cadb5622f2e13ce70efcb1d6c01a
topeojewale <topeapps@gmail.com>
2021-07-20 13:08:28 +0100

    added navigation menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  @ViewChild('navToggle') navToggle: ElementRef | undefined
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNav(event: any) {
+    this.navToggle?.nativeElement.toggle('nav-open')
   }
 
 }
