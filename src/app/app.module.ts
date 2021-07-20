9d0b2fc7e2aeb7b97fc3d677fb040aafd9f0f03c
topeojewale <topeapps@gmail.com>
2021-07-20 13:37:59 +0100

    added services section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
+import { IntroComponent } from './components/intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CardComponent
+    CardComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule
