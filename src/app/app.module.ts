18c38367f66153bb30936224daee3322ec9a481f
topeojewale <topeapps@gmail.com>
2021-07-20 13:58:21 +0100

    added about, work and footer section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { ServicesComponent } from './components/services/services.component';
+import { WorkComponent } from './components/work/work.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import { ServicesComponent } from './components/services/services.component';
     NavbarComponent,
     CardComponent,
     IntroComponent,
-    ServicesComponent
+    ServicesComponent,
+    WorkComponent,
+    AboutMeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
